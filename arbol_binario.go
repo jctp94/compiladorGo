0d5016c77b3be54cf23064d4eec17f377b0f34bb
Camilo <skamilo9406@gmail.com>
2016-11-03 17:56:58 -0500

    Se agrega la parte de asignacion de variables con el siguiente formato '1 2 + 3 4 + + x :='

--- arbol_binario.go
+++ arbol_binario.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"strings"
 	"strconv"
-//	"math/rand"
+	"bufio"
+  "os"
 )
 // arbol binario con valores enteros.
 type Arbol struct {
@@ -147,17 +148,82 @@ func hacerArbol(ent string)*Arbol{
 
 }
 
+func variables(){
+	reader := bufio.NewReader(os.Stdin)
+	//txt, _ := reader.ReadString('\n')
+	cadena:=""
+	for{
+		entrada, _ := reader.ReadString('\n')
+		cadena=cadena+""+entrada
+		if (entrada==""+"\n"){
+			break
+		}
+
+	}
+	entrada:=strings.Split(cadena, "\n")
+	entrada=entrada[:(len(entrada)-2)]
+
+
+
+	//postf:=strings.Split(operacion, " ")
+	a := make(map[int]string)
+	b := make(map[string]int)
+	for i:=0; i< len(entrada); i++{
+		separa:=strings.Split(entrada[i], ":")
+		if(separa[1]=="="){
+			separa[0]=separa[0][:(len(separa[0])-1)]
+			variable:=separa[0][(len(separa[0])-1):]
+			operacion:=separa[0][:(len(separa[0])-2)]
+			comprobar:=strings.Split(operacion, " ")
+			for j:=0; j<len(comprobar); j++{
+				_,error := strconv.Atoi(comprobar[j])
+				if(error!=nil && comprobar[j]!="+" && comprobar[j]!="-" && comprobar[j]!="*" && comprobar[j]!="/"){
+						if(len(a)>0){
+							ver:=false
+							for k:=0; k<len(a); k++{
+								if(comprobar[j]==a[k]){
+									comprobar[j]=strconv.Itoa(b[a[k]])
+									ver =true
+								}
+							}
+							if(ver==false){
+								fmt.Println("La variable: "+comprobar[j]+ " no está almacenada")
+								break
+							}
+						}else{
+							fmt.Println("La variable: "+comprobar[j]+ " no está almacenada")
+							break
+						}
+				}
+			}
+			operacion=""
+			for j:=0; j<len(comprobar); j++{
+				operacion=operacion +" " +comprobar[j]
+			}
+			operacion=operacion[1:]
+			a[len(a)]=variable
+			b[variable]=Operar(hacerArbol(operacion))
+		}
+	}
+	for i:=0; i<len(a); i++{
+		fmt.Println(b[a[i]])
+	}
+}
 func main() {
-//	var s string =""
-	//t2:= &Arbol{ &Arbol{&Arbol{nil,"5",nil}, "+", &Arbol{nil,"3",nil} }, "+", &Arbol{&Arbol{nil,"10",nil}, "*",&Arbol{nil,"3",nil} } }
-	//t3:= &Arbol{ &Arbol{&Arbol{nil,"5",nil}, "+", &Arbol{nil,"3",nil} }, "+", &Arbol{&Arbol{nil,"4",nil}, "*",&Arbol{nil,"6",nil} } }
-	//var t1 *Arbol
-//	t3:=&Arbol{nil,"5",nil}
-//t4:=&Arbol{&Arbol{nil,"-",nil},"2",&Arbol{nil,"*",nil}}
-//	fmt.Println(Operar(t4))
-	//fmt.Println(detecError(t2,""))
-	ent:= "5 6 * 5 3 * /"
-	t4:=hacerArbol(ent)
-	fmt.Println("Arbol t4: "+RecorrerInorden(t4,""))
-	fmt.Println(Operar(t4))
+/*
+var s string =""
+ t2:= &Arbol{ &Arbol{&Arbol{nil,"5",nil}, "+", &Arbol{nil,"3",nil} }, "+", &Arbol{&Arbol{nil,"10",nil}, "*",&Arbol{nil,"3",nil} } }
+ t3:= &Arbol{ &Arbol{&Arbol{nil,"5",nil}, "+", &Arbol{nil,"3",nil} }, "+", &Arbol{&Arbol{nil,"4",nil}, "*",&Arbol{nil,"6",nil} } }
+ var t1 *Arbol
+t3:=&Arbol{nil,"5",nil}
+t4:=&Arbol{&Arbol{nil,"-",nil},"2",&Arbol{nil,"*",nil}}
+fmt.Println(Operar(t4))
+ fmt.Println(detecError(t2,""))
+ent:= "5 6 * 5 3 * /"
+t4:=hacerArbol(ent)
+fmt.Println("Arbol t4: "+RecorrerInorden(t4,""))
+fmt.Println(Operar(t4))
+*/
+variables()
+
 }
